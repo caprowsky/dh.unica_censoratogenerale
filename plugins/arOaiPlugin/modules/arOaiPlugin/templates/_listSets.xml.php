c5da3e3ae90673cfaf3a5c925378c5f8459c850c
caprowsky <alessandrocapra@gmail.com>
2019-04-19 09:24:06 +0200

    refactory docker-compsoe

--- plugins/arOaiPlugin/modules/arOaiPlugin/templates/_listSets.xml.php
+++
@@ -1,8 +0,0 @@
-  <ListSets>
-    <?php foreach($oaiSets as $set): ?>
-        <set>
-           <setSpec><?php echo $set->setSpec() ?></setSpec>
-           <setName><?php echo esc_specialchars(strval($set->getName())) ?></setName>
-        </set>
-    <?php endforeach ?>
-  </ListSets>
